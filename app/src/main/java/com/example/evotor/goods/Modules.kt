fcfbe4114e46e49f0c10e6d5aca89cbd8224f049
luksoral <pletnev@midev.pro>
2022-05-12 14:38:34 +0300

    review 5

--- app/src/main/java/com/example/evotor/goods/Modules.kt
+++ app/src/main/java/com/example/evotor/goods/Modules.kt
@@ -1,19 +1,43 @@
 package com.example.evotor.goods
 
-import com.example.evotor.goods.api.RetrofitInstance
-import com.example.evotor.goods.api.provideApi
+import com.example.evotor.goods.api.Api
 import com.example.evotor.goods.database.GoodDatabase
 import com.example.evotor.goods.repository.EvotorRepository
 import com.example.evotor.goods.repository.SharedPreferencesRepository
 import com.example.evotor.goods.ui.goods.goods.GoodsListViewModel
 import com.example.evotor.goods.ui.goods.settings.SettingsViewModel
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
-    single { RetrofitInstance() }
-    single { provideApi(get()) }
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val newRequest: Request = chain.request().newBuilder()
+                    .addHeader("Authorization", Constants.TOKEN)
+                    .build()
+                chain.proceed(newRequest)
+            }.build()
+    }
+    single {
+        Retrofit.Builder()
+            .client(get())
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    single {
+        get<Retrofit>().create(Api::class.java)
+    }
+    //TODO ПРоверить, что все ок
+}
+
+val dataBaseModule = module {
     single { GoodDatabase.getDatabase(get()) }
 }
 
@@ -28,6 +52,6 @@ val viewModelModules = module {
 }
 
 fun getModules(): List<Module> {
-    return listOf(networkModule, repositoryModule, viewModelModules)
+    return listOf(networkModule, dataBaseModule, repositoryModule, viewModelModules)
 }
 
--- app/src/main/java/com/example/evotor/goods/api/Api.kt
+++ app/src/main/java/com/example/evotor/goods/api/Api.kt
@@ -5,10 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-fun provideApi(retrofitInstance: RetrofitInstance): Api {
-    return retrofitInstance.retrofit.create(Api::class.java)
-}
-
 interface Api {
     @GET("stores/{shop-uuid}/products")
     suspend fun getGoods(@Path("shop-uuid") shopUUID: String): Response<ApiItems>
